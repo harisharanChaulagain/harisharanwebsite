7c600f06aa7da72e1528a674731f647ba65b9485
Hari Sharan Chaulagain <chaulagaihari12345@gmail.com>
2023-04-24 12:13:10 +0545

    Styled Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+import Navbar from './Navbar';
+
+const Header = () => {
+  return (
+    <MainHeader>
+      <NavLink to="/">
+          <img src="./images/logo.png" alt="img" className='logo'/>
+      </NavLink>
+      <Navbar/>
+    </MainHeader>
+  )
+}
+const MainHeader = styled.header`
+padding: 0 4.8rem;
+height: 10rem;
+background-color: ${({theme}) => theme.colors.bg};
+display: flex;
+justify-content: space-between;
+align-items: center;
+
+.logo{
+  height: auto;
+  max-width: 70%;
+}
+`;
+export default Header
